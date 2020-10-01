ae176d69b780d7637c06afcedabea66e3cabda94
KAUANPEREIRA <kauanauan71@gmail.com>
2020-09-30 22:09:37 -0300

    projeto finalizado churrascometro

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
+//Carne 400 gr por pessoa +6 ==650gr
+//cerveja 1200 ml por pessoa + 6horas ==2000
+//refrigerante/agua- 1000 ml por pessoa + 6 horas 1500ml
 
 
-let n=52
-let nh=n
-console.log(nh.padEnd(9,'*'))
+let adultos= document.querySelector('#adultos')
+let criancas=document.querySelector('#criancas')
+let duracao=document.querySelector('#horarios')
+let res=document.querySelector('.res')
+
+function calcular(){
+    let a = adultos.value
+    let c= criancas.value
+    let quantidadeCarne= carnePP(duracao)* a + (carnePP(duracao) / c)
+    let quantidadeCerveja= cervejaPP(duracao)* a 
+    let quantidadeBebidas= bebidas(duracao)* a + (bebidas(duracao) / c)
+    res.innerHTML=`<p>${quantidadeCarne/1000} Kg de carne</p>`
+    res.innerHTML+=`<p>${Math.ceil(quantidadeCerveja/355)} latas de cerveja</p>`
+    res.innerHTML+=`<p>${Math.ceil(quantidadeBebidas/2000)} garrafas de refri e suco</p>`
+}
+
+function carnePP(duracao){
+    if(duracao>=6){
+        return 650
+    }else{
+        return 400
+    }
+}
+
+function cervejaPP(duracao){
+    if(duracao>=6){
+        return 2000
+    }else{
+        return 1200
+    }
+}
+function bebidas(duracao){
+    if(duracao>=6){
+        return 1500
+    }else{
+        return 1000
+    }
+}
